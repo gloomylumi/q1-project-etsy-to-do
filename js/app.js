992267353cb59a4db9455b0cf8117ee2b8ac1110
Ariana Clark <ariana.clark@gmail.com>
2016-11-02 12:15:24 -0600

    fixed unexpected token error

--- js/app.js
+++ js/app.js
@@ -1,43 +1,43 @@
 "use strict";
 $( document ).ready(
-    function() {
-      console.log( "ready" );
+  function() {
+    console.log( "ready" );
 
-      // **** EVENT LISTENERS ****
-      $( "#lulzButton" ).click( lulzButtonClicked );
-      // **** FUNCTIONS ****
-      function lulzButtonClicked() {
-        isCardPanelHidden();
-        $.ajax( {
-            url: "http://api.icndb.com/jokes/random",
-            data: {
-              escape: "javascript"
-            },
-            type: "GET",
-            dataType: "json",
-          } )
-          .done( function( json ) {
-            $( "#cardPanel" ).text( json.value.joke );
-          } )
-          .fail( function( xhr, status, errorThrown ) {
-            console.log( "Sorry, there was a problem!" );
-            console.log( "Error: " + errorThrown );
-            console.log( "Status: " + status );
-            console.dir( xhr );
-          } )
-          .always( function( xhr, status ) {
-            console.log( "The request is complete!" );
-          } );
+    // **** EVENT LISTENERS ****
+    $( "#lulzButton" ).click( lulzButtonClicked );
+    // **** FUNCTIONS ****
+    function lulzButtonClicked() {
+      isCardPanelHidden();
+      $.ajax( {
+          url: "http://api.icndb.com/jokes/random",
+          data: {
+            escape: "javascript"
+          },
+          type: "GET",
+          dataType: "json",
+        } )
+        .done( function( json ) {
+          $( "#cardPanel" ).text( json.value.joke );
+        } )
+        .fail( function( xhr, status, errorThrown ) {
+          console.log( "Sorry, there was a problem!" );
+          console.log( "Error: " + errorThrown );
+          console.log( "Status: " + status );
+          console.dir( xhr );
+        } )
+        .always( function( xhr, status ) {
+          console.log( "The request is complete!" );
+        } );
+    }
+
+    function isCardPanelHidden() {
+      if ( $( "#contentPanel" ).hasClass( "hidden" ) ) {
+        $( "#contentPanel" ).removeClass( "hidden" );
       }
-
-      function isCardPanelHidden() {
-        if ( $( "#contentPanel" ).hasClass( "hidden" ) ) {
-          $( "#contentPanel" ).removeClass( "hidden" );
-        }
-        return;
-      }
+      return;
+    }
 
 
 
 
-    );
+  } );
