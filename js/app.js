61ab9324525fcc1f326f22f167a2b837df8a404e
Ariana Clark <ariana.clark@gmail.com>
2016-11-02 11:58:22 -0600

    lulz button initial attempt

--- js/app.js
+++ js/app.js
@@ -1,4 +1,43 @@
 "use strict";
 $( document ).ready(
-  console.log( "ready" )
-);
+  function() {
+    console.log( "ready" );
+
+    // **** EVENT LISTENERS ****
+    $( "#lulzButton" ).click( lulzButtonClicked );
+    // **** FUNCTIONS ****
+    function lulzButtonClicked() {
+      isCardPanelHidden();
+      $.ajax( {
+          url: "http://api.icndb.com/jokes/random",
+          data: {
+            escape: "javascript"
+          },
+          type: "GET",
+          dataType: "json",
+        } )
+        .done( function( json ) {
+          $( "#cardPanel" ).text( json.value.joke );
+        } )
+        .fail( function( xhr, status, errorThrown ) {
+          console.log( "Sorry, there was a problem!" );
+          console.log( "Error: " + errorThrown );
+          console.log( "Status: " + status );
+          console.dir( xhr );
+        } )
+        .always( function( xhr, status ) {
+          console.log( "The request is complete!" );
+        } );
+    }
+
+    function isCardPanelHidden() {
+      if ( $( "#contentPanel" ).hasClass( "hidden" ) ) {
+        $( "#contentPanel" ).removeClass( "hidden" );
+      }
+      return;
+    }
+
+
+
+
+  } );
