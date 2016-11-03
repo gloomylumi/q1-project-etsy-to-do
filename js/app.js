b42bb7478754a46f2eff1c60b31131fd050dd23d
Ariana Clark <ariana.clark@gmail.com>
2016-11-03 10:05:11 -0600

    inspired feels button functional

--- js/app.js
+++ js/app.js
@@ -5,9 +5,10 @@ $( document ).ready(
 
     // **** EVENT LISTENERS ****
     $( "#lulzButton" ).click( lulzButtonClicked );
+    $( "#inspiredButton" ).click( inspiredButtonClicked );
     // **** FUNCTIONS ****
     function lulzButtonClicked() {
-      isCardPanelHidden();
+      prepContentPanel();
       $.ajax( {
           url: "http://api.icndb.com/jokes/random",
           data: {
@@ -32,14 +33,39 @@ $( document ).ready(
         } );
     }
 
-    function isCardPanelHidden() {
-      console.log( $( "#contentPanel" ) );
-      console.log( $( "#contentPanel" ).hasClass( "hidden" ) );
+    function inspiredButtonClicked() {
+      prepContentPanel();
+      $.ajax( {
+          url: " http://g-forismatic.herokuapp.com/api/1.0/?method=getQuote&format=json&lang=en"
+        } )
+        .done( function( json ) {
+          var quote = document.createElement( "p" );
+          var author = document.createElement( 'p' );
+          $( quote ).addClass( "quote" );
+          $( author ).addClass( "author" );
+          $( quote ).text( json.quoteText ).appendTo( "#contentPanel" );
+          console.log( $( quote ).text );
+          $( author ).text( json.quoteAuthor ).appendTo( "#contentPanel" );
+        } )
+        .fail( function( xhr, status, errorThrown ) {
+          console.log( "Sorry, there was a problem!" );
+          console.log( "Error: " + errorThrown );
+          console.log( "Status: " + status );
+          console.dir( xhr );
+        } )
+        .always( function( xhr, status ) {
+          console.log( "The request is complete!" );
+        } );
+    }
+
+
+    function prepContentPanel() {
       if ( $( "#contentPanel" ).hasClass( "hidden" ) ) {
-        console.log( "ding" );
+        // unhide the contentPanel if it is hidden
         $( "#contentPanel" ).removeClass( "hidden" );
       }
-      console.log( "dong" );
+      // clear any existing content from the content panel
+      $( "#contentPanel" ).html( '' );
       return;
     }
 
